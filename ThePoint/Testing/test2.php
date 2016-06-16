ac8231c18a0df5d2198e9b89ca6a0deab72da50d
NarmadaH <achini.nh@gmail.com>
2016-06-16 10:11:24 -0700

    phpunit test case 1

--- ThePoint/Testing/test2.php
+++ ThePoint/Testing/test2.php
@@ -6,7 +6,28 @@
  * Date: 6/16/2016
  * Time: 10:00 AM
  */
-class test2
+class test2 extends Illuminate\Foundation\Testing\TestCase
 {
+    /**
+     * The base URL to use while testing the application.
+     *
+     * @var string
+     */
+    protected $baseUrl = 'http://localhost';
+
+    /**
+     * Creates the application.
+     *
+     * @return \Illuminate\Foundation\Application
+     */
+    public function createApplication()
+    {
+        $app = require __DIR__.'/../bootstrap/app.php';
+
+        $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
+
+        return $app;
+    }
+
 
 }
