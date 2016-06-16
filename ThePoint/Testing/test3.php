bedac50e18d52862062b9a03bef232d9f8e695a3
NarmadaH <achini.nh@gmail.com>
2016-06-16 10:13:06 -0700

    test case3

---
+++ ThePoint/Testing/test3.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Achini
+ * Date: 6/16/2016
+ * Time: 10:11 AM
+ */
+class extends TestCase {
+
+    /**
+     * A basic functional test example.
+     *
+     * @return void
+     */
+    public function testDB() {
+        //Testing Database
+        $this->seeInDatabase('users', ['username' => 'admin']);
+    }
+
+    public function testURL() {
+        //Testing routing
+        $this->visit('/')
+            ->see('Sign in');
+    }
+
+    public function testIndex() {
+        //Testing index
+        $response = $this->call('GET', '/');
+
+        $this->assertEquals(200, $response->status());
+    }
+
+    public function testWarning() {
+        //Testing add Warning page
+        $response = $this->call('GET', '/addwarnings');
+
+        $this->assertEquals(200, $response->status());
+    }
+
+    public function testViewWarnings() {
+        //Testing view Warnings page
+        $response = $this->call('GET', '/viewwarnings');
+
+        $this->assertEquals(200, $response->status());
+    }
+
+}
